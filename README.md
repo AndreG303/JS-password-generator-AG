18168cc67f594b22a9ccf49cae25524e99b3e567
Andrea Gamez <andreamgamez@gmail.com>
2020-06-20 17:01:33 -0400

    Readme,style css

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # JS-password-generator-AG
+
+
+I have created a password generator using HTML, CSS and JAVASCRIPT in order to met criteria described below. 
+
+<img width="1075" alt="Screen Shot 2020-06-20 at 4 48 44 PM" src="https://user-images.githubusercontent.com/65183415/85211406-da385a00-b316-11ea-9ad5-f8f4763f58e6.png">
+
+
+Acceptance Criteria
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
--- script.js
+++ script.js
@@ -14,42 +14,23 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-// generatePassword function should be below
+// generatePassword function 
 function generatePassword() {
-  // *********************** function logic **********************
-  // THEN I am presented with a series of prompts for password criteria
-  // ??????? what series
-  // WHEN prompted for password criteria
-  // THEN I select which criteria to include in the password
-  // ????? i thought that is a given
 
-  // collected all the data here *************************
-  // ************ 1 PROMPT *************
-  // ************ need validation ***********
-  // WHEN prompted for the length of the password
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
-  // we need a prompt here and ask for the length from the user
-  // check IF the input number is < 8 and > 128 
+
+  // collection of data and validation of password lenght 
 
   var passLength = prompt(
-    "choose a password length between 8 and 128"
+    "Choose a password length between 8 and 128"
   );
   var confirmLength = parseInt(passLength);
   if (confirmLength < 8 || confirmLength > 128 || isNaN(confirmLength)) {
     alert("Error: Please generate again and choose a length btw 8 and 128");
     return;
   }
- 
-  // for (var i =8; i>= 128; i ++){
-  //  prompt("choose a length between 8 and 128")
-  // }
 
 
-
-  // WHEN I answer each prompt
-  // THEN my input should be validated and at least one character type should be selected
-  // each prompt needs to be validated...
-  // in this case, we should probably use a function to turn the length into a number
+  // Variables needed for password content 
 
   var allCharacters = [];
   var pickLowerCase = "abcdefghijklmnopqrstuvwxyz".split('');
@@ -58,13 +39,7 @@ function generatePassword() {
   var pickSpecialChar = "!@#$%^&*()+?/{-<|>".split('');
 
 
-  // ************** 4 confirms **************
-  // WHEN prompted for character types to include in the password
-  // THEN I choose lowercase, uppercase, numeric, and/or special characters
-  // we need a confirm to ask the user if they want to use lowercase
-  // we need a confirm to ask the user if they want to use uppercase
-  // we need a confirm to ask the user if they want to use numeric
-  // we need a confirm to ask the user if they want to use special characters
+  // Confirmation of data and validation of characters
 
   var confirmLowerCase = confirm(
     "Do You Want To Use Lowercase in your password?"
@@ -102,38 +77,26 @@ function generatePassword() {
     for (var i = 0; i < pickSpecialChar.length; i++) {
       allCharacters.push(pickSpecialChar[i]);
     }
-  } 
+  }
 
 
-  // if the usewr has an empty collection of characters alert
+  // Alert if the user has an empty collection of characters  
+
   if (allCharacters.length === 0) {
-    alert("You must choose at least one character type. Please generate aganin");
+    alert("You must choose at least one character type. Please generate again");
     return;
   }
 
+  // all the information is gathered (all prompts are finished getting input) Then password is generated 
 
-  // end of "collected all the data here" *************************
-  // using the number from above and the 4 booleans
-
-  // WHEN all prompts are answered
-  // THEN a password is generated that matches the selected criteria
-  // all the information is gathered (all prompts are finished getting input)
-  // all the HARD logic goes here ????????
   var endPassword = "";
-  for(var i=0; i<confirmLength; i++){
-    allCharacters[Math.floor(Math.random()*allCharacters.length)]
-    endPassword += allCharacters[Math.floor(Math.random()*allCharacters.length)]
+  for (var i = 0; i < confirmLength; i++) {
+    allCharacters[Math.floor(Math.random() * allCharacters.length)]
+    endPassword += allCharacters[Math.floor(Math.random() * allCharacters.length)]
   }
   return endPassword;
 }
-  
 
-  // WHEN the password is generated
-  // THEN the password is either displayed in an alert or written to the page
-  // either do an alert with password... or just check to see if my variable has a password
 
-  // *********************** end function logic **********************
-
-  // Add event listener to generate button
-  // WHEN I click the button to generate a password
-  generateBtn.addEventListener("click", writePassword);
+// Event listener to generate button, I click the button to generate a password
+generateBtn.addEventListener("click", writePassword);
